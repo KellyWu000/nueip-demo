fea2657ba1089454e67f2582f83fa40bad37a00e
Kelly <yijinwu5@gmail.com>
2022-02-09 22:32:58 +0800

    tooltip text  &&欄位驗証

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-var tableData =  [];
+
 
 //重新填寫 新增    
 $(".refreshVal").on("click",function(e){
@@ -9,6 +9,11 @@ $(".refreshVal").on("click",function(e){
 $(".refreshVal").on("click",function(e){
     console.log(e.target);
     $("#searchform")[0].reset();
+})
+// 重新填寫  修改
+$(".refreshVal").on("click",function(e){
+    console.log(e.target);
+    $("#modifyform")[0].reset();
 })
 
 
@@ -22,104 +27,68 @@ $(document).ready(function () {
     // 新增按鈕
     $("#addbutton").click(function (e) {
         e.preventDefault(); // avoid to execute the actual submit of the form.
-
-
         var url = "ajax/ajaxCard";
         var cnname = $("#addcnname").val();
         var enname = $("#addenname").val();
         var email =$("#email").val();
-        var phone=$("#phone").val();
+        var mobile=$("#mobile").val();
         var sex = $('input:radio:checked[name="addsex"]').val();
         var ajaxobj = new AjaxObject(url, 'json');
         ajaxobj.cnname = cnname;
         ajaxobj.enname = enname;
         ajaxobj.email = email;
-        ajaxobj.phone = phone;
+        ajaxobj.mobile = mobile;
         ajaxobj.sex = sex;
+//驗證手機號碼
+
+    if($('#mobile').val()==""){
+         $('#mobile_info').html('此欄位不得為空').css('color','red');
+       
+
+         
+      }
+       if($('#email').val()==""){
+        $('#email_info').html('此欄位不得為空').css('color','red');
+        return false;
+     }
+
+ 
+      if (!$("#mobile").val().match(/^09\d{8}$/)) {
+        $('#mobile_info').html("手機號碼格式不正確").css('color','red');
+        return false;
+     }
+    
+
+     
+
+ if (!$("#email").val().match(/^([\w\.\-]){1,64}\@([\w\.\-]){1,64}$/)) {
+    $('#email_info').html("郵箱格式不正確").css('color','red');
+    return false;
+ }
 
         ajaxobj.add();
-
-
-        
-        $("#myModal").modal({
-            // resizable: true,
-            // height: $(window).height() * 0.4,// dialog視窗度
-            // width: $(window).width() * 0.4,
-            // modal: true,
-            buttons: {
-                // 自訂button名稱
-                "新增": function (e) {
-                //    var url = "ajax/ajaxCard";
-                //     var cnname = $("#addcnname").val();
-                //     var enname = $("#addenname" ).val();
-                //     var sex = $('input:radio:checked[name="addsex"]').val();
-                //     var ajaxobj = new AjaxObject(url, 'json');
-                //     ajaxobj.cnname = cnname;
-                //     ajaxobj.enname = enname;
-                //     ajaxobj.sex = sex;
-                //     ajaxobj.add();
-
-                    e.preventDefault(); // avoid to execute the actual submit of the form.
-                },
-                // "重新填寫": function () {
-                //     $("#addform")[0].reset();
-                // },
-                // "取消": function () {
-                //     $(this).dialog("close");
-                // }
-            }
-        });
     })
-    // 搜尋按鈕
-    $("#searchbutton").click(function () {
-        $("#dialog-searchconfirm").dialog({
-            resizable: true,
-            height: $(window).height() * 0.4,// dialog視窗度
-            width: $(window).width() * 0.4,
-            modal: true,
-            buttons: {
-                // 自訂button名稱
-                "搜尋": function (e) {
-                    var url = "ajax/ajaxCard";
-                    // var data = $("#searchform").serialize();
-                    var cnname = $("#secnname").val();
-                    var enname = $("#seenname").val();
-                    var sex = $('input:radio:checked[name="sesex"]').val();
-                    var ajaxobj = new AjaxObject(url, 'json');
-                    ajaxobj.cnname = cnname;
-                    ajaxobj.enname = enname;
-                    ajaxobj.sex = sex;
-                    ajaxobj.search();
-
-                    e.preventDefault(); // avoid to execute the actual submit of the form.
-                },
-                "重新填寫": function () {
-
-                    $("#searchform")[0].reset();
-                },
-                "取消": function () {
-                    $(this).dialog("close");
-                }
-            }
-        });
-    })
+ 
   
     // 修改鈕
     $("#cardtable").on('click', '.modifybutton', function () {
         var ajaxobj = new AjaxObject(url, 'json');
         ajaxobj.modify_get();
     })
-    $("#cardtable").on('click', '.deletebutton', function () {
-        var deleteid = $(this).attr('id').substring(12);
-        var url = "ajax/ajaxCard";
-        var ajaxobj = new AjaxObject(url, 'json');
-        ajaxobj.id = deleteid;
-        ajaxobj.delete();
-    })
+ 
 
  
 });
-
+//刪除鈕
+  $('#delBtn').on('click',function(){
+    var deleteid = $(this).attr('id').substring(12);
+    var url = "ajax/ajaxCard";
+    var ajaxobj = new AjaxObject(url, 'json');
+    ajaxobj.id = deleteid;
+    ajaxobj.delete();
+  })
+    
+   
 
 
 function refreshTable(data) {
@@ -132,9 +101,12 @@ function refreshTable(data) {
         else
             strsex = '女';
         var row = $("<tr></tr>");
-        row.append($("<td></td>").html(`<a href="#" class="tooltip-test text-dark text-decoration-none"  data-toggle="tooltip" data-placement="top" title="${item.cnname}${item.enname}${strsex}">${item.cnname}
+        
+        row.append($("<td></td>").html(`<a href="#" class="tooltip-test text-dark text-decoration-none"  data-toggle="tooltip" data-placement="top" title="${item.cnname}${item.enname}-${strsex}">${item.cnname}
        </a>`));
         row.append($("<td></td>").html(item.enname));
+        row.append($("<td></td>").html(item.mobile));
+        row.append($("<td></td>").html(item.email));
         row.append($("<td></td>").html(strsex));
         row.append($("<td></td>").html('<button id="modifybutton' + item.s_sn + '"type="button" class="modifybutton btn btn-info"  data-toggle="modal"  data-target="#adjustModal" style="font-size:16px;font-weight:bold; ;outline:"none";>修改 <span class="glyphicon glyphicon-list-alt"></span></button>'));
         row.append($("<td></td>").html('<button id="deletebutton' + item.s_sn + '" type="button" class="deletebutton  btn btn-outline-danger"  data-toggle="modal"  data-target="#confirmModal" ;style="font-size:16px;font-weight:bold; ">刪除 <span class="glyphicon glyphicon-trash"></span></button>'));
@@ -147,6 +119,9 @@ function refreshTable(data) {
 
 function initEdit(response) {
   var modifyid = $("#cardtable").attr('id').substring(12);
+  $.each(response,function(key,item){
+
+  })
   $("#mocnname").val(response[0].cnname);
   $("#moenname").val(response[0].enname);
   if (response[0].sex == 0) {
@@ -158,37 +133,8 @@ function initEdit(response) {
       $("#modifywoman").prop("checked", true);
   }
   $("#modifysid").val(modifyid);
-  $("#dialog-modifyconfirm").dialog({
-      resizable: true,
-      height: $(window).height() * 0.4,// dialog視窗度
-      width: $(window).width() * 0.4,
-      modal: true,
-      buttons: {
-          // 自訂button名稱
-          "修改": function (e) {
-              $("#modifyform").submit();
-              var url = "ajax/ajaxCard";
-              var cnname = $("#mocnname").val();
-              var enname = $("#moenname").val();
-              var sex = $('input:radio:checked[name="mosex"]').val();
-              var ajaxobj = new AjaxObject(url, 'json');
-              ajaxobj.cnname = cnname;
-              ajaxobj.enname = enname;
-              ajaxobj.sex = sex;
-              ajaxobj.id = modifyid;
-              ajaxobj.modify();
+ 
 
-              e.preventDefault(); // avoid to execute the actual submit of the form.
-          },
-          "重新填寫": function () {
-              $("#modifyform")[0].reset();
-          },
-          "取消": function () {
-              $(this).dialog("close");
-          }
-      },
-      error: function (exception) { alert('Exeption:' + exception); }
-  });
 }
 
 /**
@@ -204,6 +150,8 @@ function AjaxObject(url, datatype) {
     this.datatype = datatype;
 }
 AjaxObject.prototype.cnname = '';
+AjaxObject.prototype.mobile = '';
+AjaxObject.prototype.email = '';
 AjaxObject.prototype.enname= '';
 AjaxObject.prototype.sex = '';
 AjaxObject.prototype.id = 0;
@@ -212,11 +160,12 @@ AjaxObject.prototype.alertt = function () {
 }
 
 AjaxObject.prototype.getall = function () {
-    response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","sex":"1"}]';
+  response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","mobile":"0978907345","email":"123@gmail","sex":"1"}]';
     refreshTable(JSON.parse(response));
   }
   AjaxObject.prototype.add = function () {
-    response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","sex":"1"},{"s_sn":"52","cnname":"新增帳號","enname":"NewAccount","sex":"1"}]';
+    response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","mobile":"0978907345","email":"123@gmail","sex":"1"},{"s_sn":"52","cnname":"新增帳號","enname":"NewAccount","mobile":"0978907345","email":"123@gmail","sex":"1"}]';
+  
     refreshTable(JSON.parse(response));
     $("#dialog-addconfirm").dialog("close");
   }
@@ -226,8 +175,7 @@ AjaxObject.prototype.getall = function () {
     $("#dialog-modifyconfirm").dialog("close");
   }
   AjaxObject.prototype.modify_get = function () {
-    response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","sex":"1"}]';
-    initEdit(JSON.parse(response));
+    response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","mobile":"0978907345","email":"123@gmail","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","mobile":"0978907345","email":"123@gmail","sex":"1"}]';
   }
   AjaxObject.prototype.search = function () {
     response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"}]';
@@ -235,41 +183,10 @@ AjaxObject.prototype.getall = function () {
     $("#dialog-searchconfirm").dialog("close");
   }
   AjaxObject.prototype.delete = function () {
-    response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"}]';
+    response = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0"}]';
     refreshTable(JSON.parse(response));
   }
   
-// AjaxObject.prototype.getall = function () {
-//   tableData = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","sex":"1"}]';
-//   refreshTable(JSON.parse(tableData));
-// }
-// AjaxObject.prototype.add = function () {
-
-//   tableData.push({"s_sn":"52","cnname":"新增帳號","enname":"NewAccount","sex":"1"});
-
-
-//   refreshTable(JSON.parse(tableData));
-//   $("#dialog-addconfirm").dialog("close");
-// }
-// AjaxObject.prototype.modify = function () {
-//   tableData = '[{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"}]';
-//   refreshTable(JSON.parse(tableData));
-//   $("#dialog-modifyconfirm").dialog("close");
-// }
-// AjaxObject.prototype.modify_get = function () {
-//   tableData = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"},{"s_sn":"50","cnname":"趙雪瑜","enname":"Sharon","sex":"0"},{"s_sn":"51","cnname":"賴佳蓉","enname":"Yoki","sex":"1"}]';
-//   initEdit(JSON.parse(tableData));
-// }
-// AjaxObject.prototype.search = function () {
-//   tableData = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"}]';
-//   refreshTable(JSON.parse(tableData));
-//   $("#dialog-searchconfirm").dialog("close");
-// }
-// AjaxObject.prototype.delete = function () {
-//   tableData = '[{"s_sn":"35","cnname":"邱小甘","enname":"Peter","sex":"0"},{"s_sn":"49","cnname":"蔡凡昕","enname":"Allen","sex":"0"}]';
-//   refreshTable(JSON.parse(tableData));
-// }
-
 
 
 
